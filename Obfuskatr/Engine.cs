09ee47bc7172a456b03805ee62153405bb106cce
Antoine Desportes <me@antoinedesportes.info>
2012-07-10 20:19:21 +0200

    Original commit

---
+++ Obfuskatr/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+namespace Obfuskatr
+{
+    public static class Engine
+    {
+        public static string MD5(string value)
+        {
+          Byte[] originalBytes;
+          Byte[] encodedBytes;
+          MD5 md5;
+
+          md5 = new MD5CryptoServiceProvider();
+          originalBytes = ASCIIEncoding.Default.GetBytes(value);
+          encodedBytes = md5.ComputeHash(originalBytes);
+
+          return BitConverter.ToString(encodedBytes).Replace("-", "").ToLower();
+        }
+    }
+}
---
+++ Obfuskatr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Mono.Cecil;
+namespace Obfuskatr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type the path to the file: ");
+            var path = Console.ReadLine();
+            var asmDef = AssemblyDefinition.ReadAssembly(path);
+            Console.WriteLine("[-] Assembly loaded: {0}", path);
+            Core.FullProtect(asmDef);
+            Core.WriteAssembly(asmDef);
+            Console.Write("Done!");
+            Console.ReadLine();
+        }
+    }
+}
